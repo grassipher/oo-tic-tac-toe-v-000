f2ca2526b189360c9ffb039b57e0f27131ac87f2
Gabriel Valle <gvalle91@gmail.com>
2018-06-10 19:14:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,4 +35,10 @@ class TICTACTOE
     @board[location] != " " && @board[location] != ""
   end
   
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
+  
+  
 end
